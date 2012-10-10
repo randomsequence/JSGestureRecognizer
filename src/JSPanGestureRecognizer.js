698faa7bd4ddfde18dbf309c7025b1ddb19bc8bd
Johnnie Walker <mrwalker@randomsequence.com>
2012-10-10 13:10:50 +0100

    Pan recognizer: Relative translation tracking
    Correctly track translation relative to the previous point. For the JSGestureView example, don't reset the accumulated translation.

--- src/JSPanGestureRecognizer.js
+++ src/JSPanGestureRecognizer.js
@@ -41,6 +41,7 @@ var JSPanGestureRecognizer = JSGestureRecognizer.extend({
         
         this.translation.x += p.x - this.translationOrigin.x;
         this.translation.y += p.y - this.translationOrigin.y;
+		this.translationOrigin = p;		
       }
     }
   },
